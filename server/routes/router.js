5ddd14d8dfbc9bc48b3775f95b27672a22f5583a
Anibal Montero <anibal.montero.p@gmail.com>
2022-07-11 17:59:11 +0200

    Arreglando user

--- server/routes/router.js
+++ server/routes/router.js
@@ -8,6 +8,13 @@ const {
   updateEvento,
   deleteEvento,
 } = require('../controllers/evento.controller');
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/user.controller');
 router.post('/login', login);
 router.post('/register', registro);
 router.get('/eventos', getEventos);
@@ -15,5 +22,10 @@ router.get('/evento/:id', getEvento);
 router.post('/eventos', createEvento);
 router.put('/eventos/:id', updateEvento);
 router.delete('/eventos/:id', deleteEvento);
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 module.exports = router;
