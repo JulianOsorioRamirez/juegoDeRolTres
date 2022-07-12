6832fb9c2379fcee1c10bce94435533395c87f70
julian <jurianu1214@gmail.com>
2022-07-12 15:24:18 +0200

    login

--- client/src/components/formLog.js
+++ client/src/components/formLog.js
@@ -1,12 +1,45 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import React, { useState,useEffect } from "react";
 
 function BasicExample() {
+  const [sendEmailLog, setDataToEmailLog] = useState("");
+  
+  const [sendPassLog, setDataToPassLog] = useState("");
+  console.log(sendEmailLog)
+
+  function Log () {
+    
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({email: sendEmailLog,
+                            password: sendPassLog})
+    };
+    
+    fetch("login", requestOptions)
+      .then((response) => response.json())
+      .then(res => {
+        console.log(res)
+    })
+    
+      localStorage.setItem("user", sendEmailLog)
+
+    }
+    
+    
+  
+    
+   
+        
+    
+    
+
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" onChange={(e) => setDataToEmailLog(e.target.value)} placeholder="Enter email" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -14,12 +47,12 @@ function BasicExample() {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" onChange={(e) => setDataToPassLog(e.target.value)} placeholder="Password" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary"  onClick={() => Log()} type="submit">
         Submit
       </Button>
     </Form>
